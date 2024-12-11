d40cc70e354bef1598d6ab58ae56a6fc59ad12ea
Albert Miao <amiao541769@gmail.com>
2024-12-11 11:50:12 -0800

    Minor changes to download dataset on windows

--- datasets.py
+++ datasets.py
@@ -8,16 +8,18 @@ def generate_data_loaders(opt):
     if opt.dataset == 'MNIST':
         transform = transforms.Compose([transforms.ToTensor(),
                                         transforms.Normalize((0.5), (0.5))])
+            
+        trainset = torchvision.datasets.MNIST(root='../data', train=True,
+                                            download=True, transform=transform)
+
+        testset = torchvision.datasets.MNIST(root='../data', train=False,
+                                            download=True, transform=transform)
         
-        trainset = torchvision.datasets.MNIST(root='./data', train=True,
-                                              download=True, transform=transform)
+            
         trainloader = torch.utils.data.DataLoader(trainset, batch_size=opt.batch_size,
-                                                  shuffle=True, num_workers=2)
-        
-        testset = torchvision.datasets.MNIST(root='./data', train=False,
-                                             download=True, transform=transform)
+                                        shuffle=True, num_workers=2)
         testloader = torch.utils.data.DataLoader(testset, batch_size=1,
-                                                shuffle=False, num_workers=2)
+                                        shuffle=False, num_workers=2)
         
         return trainloader, testloader
     
